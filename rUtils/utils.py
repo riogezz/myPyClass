7ff3dd4e9290875507b06ef442812baa4bc1fef5
Sergio Cricca <sergio.cricca@gmail.com>
2019-10-01 08:47:49 +0200

    fix

--- rUtils/utils.py
+++ rUtils/utils.py
@@ -38,7 +38,7 @@ class utils():
             with open(filename) as f:
                 return yaml.safe_load(f)
         except Exception as e:
-            logging.error("Error: "+str(e))
+            logging.error(str(e))
 
     @staticmethod
     def readJSON(filename):
@@ -46,14 +46,14 @@ class utils():
             with open(filename) as f:
                 return json.load(f)
         except Exception as e:
-            logging.error("Error: "+str(e))
+            logging.error(str(e))
     
     @staticmethod
     def convertedFile(filename):
         try:
             return munchify(__class__.readFile(filename))
         except Exception as e:
-            logging.error("Error: "+str(e))
+            logging.error(str(e))
 
     @staticmethod
     def applyFilter(inputA, filterlist):
@@ -72,7 +72,7 @@ class utils():
                 output.extend(list(filter(f1.match, inputlist)))
             return output
         except Exception as e:
-            logging.error("Error: "+str(e))
+            logging.error(str(e))
             
 
     @staticmethod
