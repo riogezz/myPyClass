f3ba8fc70e34a4f65981e65723f2b5aa128dae83
Sergio Cricca <sergio.cricca@gmail.com>
2019-12-08 21:09:22 +0100

    updates v0.0.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
     name='rUtils',
-    version='0.0.8',
+    version='0.0.9',
     description='my general tools',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
@@ -16,7 +16,7 @@ setup_args = dict(
     packages=find_packages(),
     author='Sergio Cricca',
     author_email='sergio.cricca@gmail.com',
-    keywords=['rUtils', 'utils', 'scheduler', 'elastic'],
+    keywords=['rUtils', 'utils', 'scheduler', 'Elasticsearch', 'elastic'],
     url='https://github.com/riogezz/myPyClass',
     download_url=''
 )
